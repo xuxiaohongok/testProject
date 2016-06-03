bf67b1b75b46ef63bcc6ba70dfb0b79663fbf98d
xuxiaohongok <749881273@qq.com>
2016-06-03 20:05:27 +0800

    ok

--- zs-engine - 副本/src/main/java/com/zhidian/ad/service/AdSearchService.java
+++
@@ -1,7 +0,0 @@
-package com.zhidian.ad.service;
-
-public interface AdSearchService {
-	public String adSearchHander(String adMessage);
-	
-	public String adSearchHanderV1(String adMessage);
-}
--- zs-engine - 副本/src/main/java/com/zhidian/dsp/solr/service/SolrSearchAdService.java
+++
@@ -1,9 +0,0 @@
-package com.zhidian.dsp.solr.service;
-
-import com.zhidian.dsp.vo.solr.SearchAd;
-import com.zhidian.dsp.vo.solr.SearchAdCondition;
-import com.zhidian.dsp.vo.solr.SearchAdMateriolCondition;
-
-public interface SolrSearchAdService {
-	public SearchAd searchAdFormSolr(SearchAdCondition ad, SearchAdMateriolCondition searchAdMateriolCondition);
-}
